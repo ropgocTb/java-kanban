a2d3f304aa2cafbb7a4d1eecec5dcab3b8d968fd
ropgocTb <216320907+ropgocTb@users.noreply.github.com>
2025-09-23 13:23:03 +0300

    Changed: fromString() now uses Optional to avoid NPE, changed imports

--- src/manager/CSVTaskFormat.java
+++ src/manager/CSVTaskFormat.java
@@ -1,6 +1,12 @@
 package manager;
 
-import tasks.*;
+import tasks.Task;
+import tasks.Epic;
+import tasks.SubTask;
+import tasks.TaskType;
+import tasks.TaskStatus;
+
+import java.util.Optional;
 
 public class CSVTaskFormat {
     public static String toString(Task task) {
@@ -17,7 +23,7 @@ public class CSVTaskFormat {
         return "id,type,name,status,description,epic";
     }
 
-    public static Task fromString(String str) {
+    public static Optional<Task> fromString(String str) {
         String[] fields = str.split(",");
         int id = Integer.parseInt(fields[0]);
         TaskType type = TaskType.valueOf(fields[1]);
@@ -27,32 +33,32 @@ public class CSVTaskFormat {
         int epicId = fields.length > 5 ? Integer.parseInt(fields[5]) : -1;
 
         switch (type) {
-            case TaskType.TASK: {
+            case TASK: {
                 Task task = new Task(title, description);
                 task.setId(id);
                 task.setType(type);
                 task.setStatus(status);
-                return task;
+                return Optional.of(task);
             }
-            case TaskType.EPIC: {
+            case EPIC: {
                 Epic epic = new Epic(title, description);
                 epic.setId(id);
                 epic.setType(type);
                 epic.setStatus(status);
-                return epic;
+                return Optional.of(epic);
             }
-            case TaskType.SUBTASK: {
+            case SUBTASK: {
                 SubTask subTask = new SubTask(title, description);
                 subTask.setId(id);
                 subTask.setType(type);
                 subTask.setStatus(status);
                 subTask.setParent(epicId);
-                return subTask;
+                return Optional.of(subTask);
             }
             default:
                 System.out.println("Нет такого типа задачи");
         }
 
-        return null;
+        return Optional.empty();
     }
 }
