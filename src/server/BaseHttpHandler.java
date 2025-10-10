4fc46b9639d275e046393bf354fa524541a67bdc
ropgocTb <216320907+ropgocTb@users.noreply.github.com>
2025-10-10 20:08:27 +0300

    New: HTTP API for TaskManager

---
+++ src/server/BaseHttpHandler.java
@@ -0,0 +1,49 @@
+package server;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public abstract class BaseHttpHandler implements HttpHandler {
+    protected void sendText(HttpExchange h, String text) throws IOException {
+        byte[] resp = text.getBytes(StandardCharsets.UTF_8);
+        h.getResponseHeaders().add("Content-Type", "application/json;charset=utf-8");
+        h.sendResponseHeaders(200, resp.length);
+        h.getResponseBody().write(resp);
+        h.close();
+    }
+
+    protected void sendNotFound(HttpExchange h) throws IOException {
+        String response = "Not found";
+        byte[] resp = response.getBytes(StandardCharsets.UTF_8);
+        h.getResponseHeaders().add("Content-Type", "text/plain;charset=utf-8");
+        h.sendResponseHeaders(404, resp.length);
+        h.getResponseBody().write(resp);
+        h.close();
+    }
+
+    protected void sendHasOverlaps(HttpExchange h) throws IOException {
+        String response = "Not Acceptable";
+        byte[] resp = response.getBytes(StandardCharsets.UTF_8);
+        h.getResponseHeaders().add("Content-Type", "text/plain;charset=utf-8");
+        h.sendResponseHeaders(406, resp.length);
+        h.getResponseBody().write(resp);
+        h.close();
+    }
+
+    protected void sendInvalidInput(HttpExchange h) throws IOException {
+        String response = "Not Acceptable Input";
+        byte[] resp = response.getBytes(StandardCharsets.UTF_8);
+        h.getResponseHeaders().add("Content-Type", "text/plain;charset=utf-8");
+        h.sendResponseHeaders(500, resp.length);
+        h.getResponseBody().write(resp);
+        h.close();
+    }
+
+    protected void sendCreated(HttpExchange h) throws IOException {
+        h.sendResponseHeaders(201, -1);
+        h.close();
+    }
+}
